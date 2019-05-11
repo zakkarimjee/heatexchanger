97403b160c46af1cc21a482780ca68b075bae1dc
M.J.D. Adams <mja77@cam.ac.uk>
2019-05-11 22:17:46 +0100

    delete Mel

--- Mel.m
+++
@@ -1,9 +0,0 @@
-Y = 0.014; %variable
-
-L = 0.35; %varaible needs to be below 0.3 
-NoB = 9; %variable number of baffles
-NoT = 13; %varaible number of tubes
-
-a = 0.34; %for square (0.2 for triangular)
-
-[dp_cold, Re_cold] = dp_hx_cold2(0.5, L, NoT, NoB, Y, a)
