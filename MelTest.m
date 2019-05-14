e94ff9b577655c2632a69956e60a79290c29afba
Mel222 <mja77@cam.ac.uk>
2019-05-14 17:59:40 +0100

    almost finish multi shell

--- MelTest.m
+++ MelTest.m
@@ -1,3 +1,4 @@
+close 
 clear
 
 init
@@ -10,6 +11,8 @@ NoB = 14; %baffles
 % Y = 0.0156; %m pitch
 Y = 0.012; %m pitch
 a = 0.34; %square pitch, 0.2 for triangular
+n_shell = 2;
+n_tube = 4;
 
 mc = fzero(@dp_diff_cold,[0.1 0.9]);
 mh = fzero(@dp_diff_hot,[0.1 0.9]);
@@ -17,6 +20,8 @@ mh = fzero(@dp_diff_hot,[0.1 0.9]);
 [dp_hot, Re_hot] = dp_hx_hot(mh,L,NoT);
 [dp_cold, Re_cold] = dp_hx_cold(mc,L,NoT,NoB,Y,a);
 
-[Q, ThOut, TcOut, Eff] = thermal(mh,mc,Re_hot,Re_cold,L,NoT)
+[Q, ThOut, TcOut, Eff] = thermal(mh,mc,Re_hot,Re_cold,L,NoT);
+[Q, ThOut, TcOut, Eff, R, P, F] = thermal2(mh,mc,Re_hot,Re_cold,L,NoT);
+
 
 
--- thermal3.m
+++ thermal3.m
@@ -1,6 +1,6 @@
-function [SolQ, SolThOut, SolTcOut, Eff, SolR, SolP, SolF] = thermal2(mh,mc,Re_hot,Re_cold,L,NoT)
+function [SolQ, SolThOut, SolTcOut, Eff, SolR, SolP, SolF] = thermal3(mh,mc,Re_hot,Re_cold,L,NoT,n)
 
-global d0 di ThIn TcIn cp Pr K Ktube c
+global d0 di ThIn TcIn cp Pr K Ktube c 
 
 Nui = 0.023*Re_hot^0.8*Pr^0.3;
 hi = Nui*K/di;
@@ -12,7 +12,7 @@ A = pi*di*L*NoT;
 syms Q ThOut TcOut R P F
 
 eqns = [mh*cp*(ThIn - ThOut)==Q, mc*cp*(TcOut-TcIn)==Q, Q==U*A*F*(((ThIn-TcOut)-(ThOut-TcIn))/log((ThIn-TcOut)/(ThOut-TcIn))),R==(TcIn-TcOut)/(ThOut-ThIn),P==(ThOut-ThIn)/(TcIn-ThIn),...
-F==((R^2+1)^0.5)*log((1-P)/(1-R*P))/((R-1)*log(((1-P)+(1-R*P)+P*((R^2+1)^0.5))/((1-P)+(1-R*P)-P*((R^2+1)^0.5))))];
+F==((R^2+1)^0.5)*log(((1-R*P)/(1-P))^(1/n))/((R-1)*log(((1-P)+(1-R*P)+P*((R^2+1)^0.5))/((1-P)+(1-R*P)-P*((R^2+1)^0.5))))];
 vars = [Q,ThOut,TcOut,R,P,F];
 [SolQ, SolThOut, SolTcOut, SolR, SolP, SolF] = vpasolve(eqns,vars);
 
